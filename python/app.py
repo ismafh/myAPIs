8acba2efa4338e464181bf836f70748d7e15b3c6
Ismael Fernández Herreruela <ismaelfh.2001@gmail.com>
2024-08-19 00:46:35 +0200

    /tasks endpoint

--- python/app.py
+++ python/app.py
@@ -41,6 +41,11 @@ def create_task():
 
     return task_schema.jsonify(new_task)
 
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    all_tasks = Task.query.all()
+    result = tasks_schema.dump(all_tasks)
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
